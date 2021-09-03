539267e3fade8790abca8449d6e5a2b3ac992324
Ross Phillips <ross+dev@cloudcannon.com>
2021-09-03 19:09:34 +1200

    Updated 'build-tools/afterBuild.js' via CloudCannon

--- build-tools/afterBuild.js
+++ build-tools/afterBuild.js
@@ -1,4 +1,4 @@
-import { CMS } from '../config/_siteConfig'
-const cms = new CMS()
-const runAfter = require(`${__dirname}/../cms/${cms.slug}/hooks/afterBuild`).default
-runAfter()
+// import { CMS } from '../config/_siteConfig'
+// const cms = new CMS()
+// const runAfter = require(`${__dirname}/../cms/${cms.slug}/hooks/afterBuild`).default
+// runAfter()
